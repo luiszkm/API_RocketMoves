68a1ca4c2bfc4c7c7fa48a8d6ed878a0463dd683
Luis Murilo <luiszkm@gmail.com>
2022-06-16 14:36:16 -0300

    create migration

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,5 +5,6 @@ const UserController = require("../controllers/UsersController")
 const userController = new UserController()
 
 userRoutes.post("/", userController.create)
+userRoutes.put("/:id", userController.update)
 
 module.exports = userRoutes
