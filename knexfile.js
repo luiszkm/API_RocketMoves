5ddd2d79500bdd2a36ed8678539b4bcc7972b628
Luis Murilo <luiszkm@gmail.com>
2022-06-16 16:13:21 -0300

    finish migrate config

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname,"src", "database", "database.db")
     },
-    useNullASDefault: true
+    migrations:{
+      directory: path.resolve(__dirname,"src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
   },
 };
