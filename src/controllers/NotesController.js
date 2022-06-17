e8964fbab0a11c9cf6df37f0ad6d36bc9de05a77
Luis Murilo <luiszkm@gmail.com>
2022-06-17 13:55:57 -0300

    finish notes controller

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -36,10 +36,49 @@ class NotesController {
   }
 
   async delete(request, response) {
-    const { id} = request.params;
-    await knex("movies_notes").where({id}).delete()
+    const { id } = request.params;
+    await knex("movies_notes").where({ id }).delete()
 
     return response.json();
+  }
+
+  async index(request, response) {
+    const { title, user_id, tags } = request.query
+
+    let notes
+    if (tags) {
+      const filterTags = tags.split(',').map(tags => tags.trim())
+
+      notes =await knex("movie_Tags")
+      .select([
+        "movies_notes.id",
+        "movies_notes.title",
+        "movies_notes.user_id"
+      ])
+      .where("movies_notes.user_id", user_id)
+      .whereLike("movies_notes.title",`%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("movies_notes", "movies_notes.id", "movie_Tags.note_id")
+      .orderBy("movies_notes.title")
+
+    } else {
+
+      notes = await knex("movies_notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title")
+    }
+
+    const userTags = await knex("movie_Tags").where({ user_id })
+    const notesWithTags = await notes.map(note =>{
+
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+    return response.json({ notesWithTags })
   }
 }
 
